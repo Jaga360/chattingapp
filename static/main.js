39931ca40f7062c8cdde25336c23f916b6b9a424
Jaga360 <kingsmanjagadeesh@gmail.com>
2021-03-21 14:32:03 +0530

    setup socketio at the frontend

--- static/main.js
+++ static/main.js
@@ -1,4 +1,10 @@
 
+
+ var socket = io.connect(location.protocol + '//' + document.domain +':' + location.port );
+ socket.on('connect', function() {
+     socket.emit('my event', {data: 'I\'m connected!'});
+ });
+
 function send(){
    sendBox=document.getElementById("msgBox")
    console.log(sendBox.value)
