edc9c85a47312fb3ea3d49ca426f4ee84cca1b3f
Jaga360 <kingsmanjagadeesh@gmail.com>
2021-03-21 14:21:31 +0530

    setup socketio at the server end

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run()
+   socketio.run(app)
