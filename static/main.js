211cf37a8879f362a8120e70343d046a50f8f5ce
Jaga360 <kingsmanjagadeesh@gmail.com>
2021-03-21 14:39:03 +0530

    modified send function to send msg from client to server

--- static/main.js
+++ static/main.js
@@ -1,12 +1,12 @@
 
 
  var socket = io.connect(location.protocol + '//' + document.domain +':' + location.port );
- socket.on('connect', function() {
-     socket.emit('my event', {data: 'I\'m connected!'});
- });
+//  socket.on('connect', function() {
+//      socket.emit('my event', {data: 'I\'m connected!'});
+//  });
 
 function send(){
    sendBox=document.getElementById("msgBox")
-   console.log(sendBox.value)
+   socket.emit('msg',sendBox.value)
    sendBox.value=""
 };
