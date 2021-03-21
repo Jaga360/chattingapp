c45bf374d03b9d814626dd326431f86ce7e31913
Jaga360 <kingsmanjagadeesh@gmail.com>
2021-03-21 15:33:52 +0530

    resolved msg not reachable to serve bug

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+# socketio = SocketIO(app,cors_allowed_origins="http://localhost") 
 
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+# @socketio.on("msg")
+# def handle_msg(data):
+#     socketio.emit('push',data, broadcast=True, include_self = False)
+
 
 if __name__ == '__main__':
    socketio.run(app)
--- static/main.js
+++ static/main.js
@@ -1,12 +1,16 @@
 
-
- var socket = io.connect(location.protocol + '//' + document.domain +':' + location.port );
+// var socket = io();
+var socket = io.connect(location.protocol + '//' + document.domain +':' + location.port);
 //  socket.on('connect', function() {
 //      socket.emit('my event', {data: 'I\'m connected!'});
 //  });
 
+// socket.on("push",function(data){
+//    console.log(data)
+// })
+
 function send(){
    sendBox=document.getElementById("msgBox")
-   socket.emit('msg',sendBox.value)
+   socket.emit("msg","hello")
    sendBox.value=""
 };
